jiceher/oneapi-python

commit 286f1cfc7f8239b11dcf2cf79809408b56a446cc

Fix client_correlator inside response
Signed-off-by: JiCeheR <jiceher@free.fr>

oneapi/models.py
@@ -85,7 +85,7 @@ class ResourceReference(mod_object.AbstractModel):
     # The client correlator for this message. This value may be used to query 
     # for message status later.
     sender = mod_object.GetPartsOfUrlFieldConverter('resourceReference.resourceURL', -3)
-    client_correlator = mod_object.GetPartsOfUrlFieldConverter('resourceReference.resourceURL', -1)
+    client_correlator = mod_object.FieldConverter('clientCorrelator')
 
     def __init__(self, client_correlator=None, sender=None):
         mod_object.AbstractModel.__init__(self)

commit 37860df45237934e3f5ea959c995643a4cbc7fdb

Fix query delivery status
Fix regression introduced by 286f1cfc7f8239b11dcf2cf79809408b56a446cc
The ResourceURL does not content Client Correlator but Request ID to use in
delivery query status

Signed-off-by: JiCeheR <jiceher@free.fr>

example_send_flash_message_and_check_status.py
@@ -59,15 +59,15 @@ if not result.is_success():
 
 print result
 # store client correlator because we can later query for the delivery status with it:
-client_correlator = result.client_correlator
+request_id = result.request_id
 print 'Is success = ', result.is_success()
 print 'Sender = ', result.sender
-print 'Client correlator = ', result.client_correlator
+print 'Request ID = ', result.request_id
 
 # Few seconds later we can check for the sending status
 time.sleep(10)
 
 # example:query-for-delivery-status
-query_status = sms_client.query_delivery_status(client_correlator, args.sender)
+query_status = sms_client.query_delivery_status(request_id, args.sender)
 delivery_status = query_status.delivery_info[0].delivery_status
 # ----------------------------------------------------------------------------------------------------

example_send_logo_message_and_check_status.py
@@ -71,15 +71,15 @@ if not result.is_success():
 
 print result
 # store client correlator because we can later query for the delivery status with it:
-client_correlator = result.client_correlator
+request_id = result.request_id
 print 'Is success = ', result.is_success()
 print 'Sender = ', result.sender
-print 'Client correlator = ', result.client_correlator
+print 'Request ID = ', result.request_id
 
 # Few seconds later we can check for the sending status
 time.sleep(10)
 
 # example:query-for-delivery-status
-query_status = sms_client.query_delivery_status(client_correlator, args.sender)
+query_status = sms_client.query_delivery_status(request_id, args.sender)
 delivery_status = query_status.delivery_info[0].delivery_status
 # ----------------------------------------------------------------------------------------------------

example_send_message_and_check_status.py
@@ -59,15 +59,15 @@ if not result.is_success():
 
 print result
 # store client correlator because we can later query for the delivery status with it:
-client_correlator = result.client_correlator
+request_id = result.request_id
 print 'Is success = ', result.is_success()
 print 'Sender = ', result.sender
-print 'Client correlator = ', result.client_correlator
+print 'Request ID = ', result.request_id
 
 # Few seconds later we can check for the sending status
 time.sleep(10)
 
 # example:query-for-delivery-status
-query_status = sms_client.query_delivery_status(client_correlator, args.sender)
+query_status = sms_client.query_delivery_status(request_id, args.sender)
 delivery_status = query_status.delivery_info[0].delivery_status
 # ----------------------------------------------------------------------------------------------------

example_send_message_and_wait_for_dlr_push.py
@@ -67,7 +67,7 @@ if not result.is_success():
 
 print result
 print 'Is success = ', result.is_success()
-print 'Client correlator = ', result.client_correlator
+print 'Request ID = ', result.request_id
 
 # Wait for 15 seconds for push-es
 server = dummyserver.DummyWebServer(port)

example_send_ringtone_message_and_check_status.py
@@ -65,15 +65,15 @@ if not result.is_success():
 
 print result
 # store client correlator because we can later query for the delivery status with it:
-client_correlator = result.client_correlator
+request_id = result.request_id
 print 'Is success = ', result.is_success()
 print 'Sender = ', result.sender
-print 'Client correlator = ', result.client_correlator
+print 'Request ID = ', result.request_id
 
 # Few seconds later we can check for the sending status
 time.sleep(10)
 
 # example:query-for-delivery-status
-query_status = sms_client.query_delivery_status(client_correlator, args.sender)
+query_status = sms_client.query_delivery_status(request_id, args.sender)
 delivery_status = query_status.delivery_info[0].delivery_status
 # ----------------------------------------------------------------------------------------------------

oneapi/models.py
@@ -85,13 +85,13 @@ class ResourceReference(mod_object.AbstractModel):
     # The client correlator for this message. This value may be used to query 
     # for message status later.
     sender = mod_object.GetPartsOfUrlFieldConverter('resourceReference.resourceURL', -3)
-    client_correlator = mod_object.FieldConverter('clientCorrelator')
+    request_id = mod_object.GetPartsOfUrlFieldConverter('resourceReference.resourceURL', -1)
 
-    def __init__(self, client_correlator=None, sender=None):
+    def __init__(self, request_id=None, sender=None):
         mod_object.AbstractModel.__init__(self)
 
         self.sender = sender
-        self.client_correlator = client_correlator
+        self.request_id = request_id
 
 # ----------------------------------------------------------------------------------------------------
 